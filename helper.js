f2f462b21d383f90adab3b51af28af2e9f86edbd
lougeniaC64 <lougenia.bailey@gmail.com>
2017-04-24 15:37:41 -0400

    Added code to the getImageFile function for retrieving image data for the selected local file

--- helper.js
+++ helper.js
@@ -1,5 +1,11 @@
-function getImageFile() {
-    console.log('get file function')
+
+function getImageFile(file, callback) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => { callback(reader.result) }, false)
+
+    if (file) {
+        reader.readAsDataURL(file)
+    }
 }
 
 export { getImageFile }
